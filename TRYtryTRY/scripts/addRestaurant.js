120e1038f9d14fc593c4120c3c9d77041d49c320
Rumyana Bankina <r_bankina@yahoo.com>
2013-10-13 09:08:01 -0500

    Changes made by Rumyana Bankina (r_bankina@yahoo.com).

--- TRYtryTRY/scripts/addRestaurant.js
+++ TRYtryTRY/scripts/addRestaurant.js
@@ -9,6 +9,14 @@ document.addEventListener("deviceready", function () {
     		localStorage.setItem(name, name);
             localStorage.setItem(name+"-address", address);
             localStorage.setItem(name+"-picture", "PIC");
+            navigator.notification.vibrate(300);
+            navigator.notification.alert("Restaurant Added!");
+            app.application.navigate("#restaurants-view");
+            
+            
+            //navigator.notification.vibrate(300);
+            //app.application.navigate("pages/feedbackPage/feedbackPage.html#feedbackPage-page")
+            
             
             //localStorage.setItem(name, {"address" : address, "picture" : "PIC_URL"});
             //napravi navigaziq kam list s resturant

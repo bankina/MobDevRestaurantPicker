435eb0e12599734eddab4d101a71b97c6a7c516e
Rumyana Bankina <r_bankina@yahoo.com>
2013-10-13 08:37:22 -0500

    Changes made by Rumyana Bankina (r_bankina@yahoo.com).

--- TRYtryTRY/scripts/notifications.js
+++ TRYtryTRY/scripts/notifications.js
@@ -1,1 +1,27 @@
-﻿
+var app = app || {};
+
+(function(a) {    
+    a.notifications = {
+        init:function() {
+        },
+        close: function() {            
+        },
+        alert: function() {
+            navigator.notification.alert("Alert!");
+        },
+        confirm: function() {
+            navigator.notification.confirm("Are you sure?!");
+        },
+        prompt:function() {
+            navigator.notification.prompt("How old are you", function(result) {               
+                navigator.notification.alert(result); 
+            });
+        },
+        beep: function() {
+            navigator.notification.beep(3);
+        },
+        vibrate: function() {
+            navigator.notification.vibrate(2000);
+        }
+    };
+}(app));
